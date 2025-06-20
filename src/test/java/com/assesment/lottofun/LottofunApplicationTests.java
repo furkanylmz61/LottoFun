66d6ab6bdfa428be17f54c288bfa943844e0247b
Ömer Furkan Yılmaz <furkanylm30@gmail.com>
2025-06-20 23:57:57 +0300

    initial commit

---
+++ src/test/java/com/assesment/lottofun/LottofunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assesment.lottofun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottofunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
