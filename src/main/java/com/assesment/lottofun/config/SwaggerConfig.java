a6abefce87e923e22d1030355ffc4a4b0782eb54
Ömer Furkan Yılmaz <furkanylm30@gmail.com>
2025-06-22 15:34:48 +0300

    add Swagger configuration for OpenAPI documentation

---
+++ src/main/java/com/assesment/lottofun/config/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.assesment.lottofun.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("LottoFun API")
+                        .description("Lottery Game Backend API - A comprehensive lottery system with user management, ticket purchasing, and automated draw execution")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("LottoFun Development Team")
+                                .email("dev@lottofun.com")
+                                .url("https://lottofun.com"))
+                        .license(new License()
+                                .name("MIT License")
+                                .url("https://opensource.org/licenses/MIT")))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .description("Enter JWT token here")));
+    }
+}
